077d8ff6797c89443dd1a7489000499c1627b4e2
Hazarsoysuren <hazar.soysuren1@gmail.com>
2024-02-14 14:12:24 +0000

    Added js functions to the become a member

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 
-    // Add event listener to form submission
-    document.getElementById("myForm").addEventListener("submit", function(event) {
-        // Prevent default form submission behavior
-        event.preventDefault();
-
-        // Display the response message
-        document.getElementById("response-message").style.display = "block";
-        document.getElementById("form-text").style.display = "none";
-    });
+function setAction(form) {
+    document.getElementById("response-message").style.display = "block";
+    document.getElementById("form-text").style.display = "none";
+    document.getElementById("myForm").style.display = "none";
+    return false;
+  }
