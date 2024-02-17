6cc3b105da8ddd73b56ff1be27c11e3a63dd2f40
Hazarsoysuren <hazar.soysuren1@gmail.com>
2024-02-17 09:33:21 +0000

    Added some functions

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 
 function setAction(form) {
-    document.getElementById("response-message").style.display = "block";
-    document.getElementById("form-text").style.display = "none";
-    document.getElementById("myForm").style.display = "none";
-    return false;
-  }
+  document.getElementById("response-message").style.display = "block";
+  document.getElementById("form-text").style.display = "none";
+  document.getElementById("myForm").style.display = "none";
+  return false;
+}
+
+function setAction(form) {
+  event.preventDefault(); // Prevent the default form submission behavior
+  // Get the button element
+  var button = form.querySelector("button");
+  // Replace the button with a text node saying "Sent"
+  button.parentNode.replaceChild(document.createTextNode("Sent"), button);
+  return false;
+}
 
   const navToggle = document.getElementById("nav-toggle");
 const navLinks = document.querySelector(".nav-links");
